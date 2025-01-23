9c1706ead72de0389da14fe795c493a995695d30
momaek <momaek17@gmail.com>
2025-01-23 10:56:17 +0800

    init project

---
+++ LazyMeeting/LazyMeetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyMeetingApp.swift
+//  LazyMeeting
+//
+//  Created by wentx on 2025/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyMeetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
